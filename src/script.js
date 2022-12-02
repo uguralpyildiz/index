fbad0f227e7d83448d852b31dd4c32388ff902ed
Ugur Alp Yildiz <77773407+uguralpyildiz@users.noreply.github.com>
2022-12-02 14:50:20 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,4 @@
-window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-}
+document.body.scrollTop = document.documentElement.scrollTop = 0;
 
 
 const texts = ['Front-end Developer', 'UI & UX Developer', 'React Developer'];
